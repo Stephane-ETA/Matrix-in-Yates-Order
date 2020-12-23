9bd5b99a02b7fd70afcf166b0660ce368f948a8c
Stephane-ETA <a06722019@protonmail.com>
2020-12-23 17:59:09 +0100

    Implement method to file the matrix according to yates order

--- Sources/src/Matrix.cpp
+++ Sources/src/Matrix.cpp
@@ -44,3 +44,55 @@ std::vector<std::vector<int> >  Matrix::getCombinations()
     }
     return allCombi;
 }
+
+void Matrix::fillMatrix()
+{
+     int signHint;
+
+
+  //fill matrix with  1
+    for(int i=0;i<numberOfExperiment;i++)
+ {
+     for(int j=0;j<numberOfColumn;j++)
+     {
+        matrix[i][j]=1;
+     }
+ }
+
+
+   //factors columns
+    for( int i=0;i<numberOfExperiment;i++)
+    {
+        for(int j=1;j<=numberOfFactor;j++)
+        {
+            int nodivi=(int)pow(2,(j-1));
+            int div=(int)((i+1)/nodivi);
+            int rest;
+            if(j==1){rest =0;}
+            else {rest=((i+1) % nodivi ==0? 0:1);}
+            signHint=div+rest;
+            if(signHint% 2==0)
+            {
+             this->matrix[i][j]=1;
+            }
+            else
+            {
+               this->matrix[i][j]=-1;
+            }
+        }
+    }
+
+    //interactions columns
+    std::vector<std::vector<int> > combi = getCombinations();
+    for(int j =0 ; j<combi.size();j++)
+    {
+        std::vector<int> oneCombi=combi[j];
+        for(int k=0;k<oneCombi.size();k++){
+        for(int i =0;i<numberOfExperiment;i++)
+        {
+            matrix[i][numberOfFactor+j+1]*=matrix[i][oneCombi[k]];
+        }
+        }
+    }
+
+}
